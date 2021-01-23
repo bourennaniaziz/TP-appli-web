d1a3e5e37ddf74a3c8226b87c0ef152ad73fde7e
bourennaniaziz <73060054+bourennaniaziz@users.noreply.github.com>
2021-01-23 19:03:56 +0100

    Add files via upload

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -1,37 +1,67 @@
-window.onload = function () {
-  document.querySelector("#name").addEventListener("keyup", function (event) {
-    document.getElementById("nameCounter").textContent = `${event.target.value.length} car.`;
-  });
+$(document).ready(function () {
+    // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+    console.log("DOM ready!");
+
+     // Afficher le nombre de caractère saisie (jQuery)
+    $("#name").keyup(function () {
+        
+      $("#car").text($("#name").val().length + " car.");
+
+}); 
+
+    $("#firstname").keyup(function () {
+    
+      $("#car1").text($("#firstname").val().length + " car.");
+
+});
+
+    // Y mettre le code jQuery pour valider tous les champs du formulaire
+
+    var error = "";
+
+    var champsList = document.querySelectorAll(".form-control");
+
+    $("#valider").on("click", function (event) {
+        event.preventDefault();
+        error = "";
+
+        champsList.forEach(function (champItem) {
+            if (champItem.value.length == 0) {
+             error += "Champs vide : " + document.querySelector("[for='"+champItem.id+"']").textContent + "<br/>" ;
+            }
+
+        });
 
 
-  document.querySelector("#gps").addEventListener("click", function (event) {
-    event.preventDefault();
-    getLocation();
-  });
+        if (error =="") {
+          console.log('ACTION::ADD'); 
+          let firstname = $('#firstname').val();
+          let name = $('#name').val();
+          let address = $('#adresse').val();
+          let birthday = $('#birth').val();
+          let mail = $('#mail').val();
+        
+          document.querySelector("table tbody").innerHTML = document.querySelector("table tbody").innerHTML +
+              '<tr><td>' + name + '</td><td>' + firstname + '</td><td>' + birthday + '</td><td>' + address + '</td><td>' + mail + '</td><td>';
+            
+          console.log('ACTION::getList'); 
+          for(var index in contactList){
+            console.log(contactList[index].name);
+          }
 
-  document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    contactStore.add(
-      document.querySelector("#name").value,
-      document.querySelector("#firstname").value,
-      document.querySelector("#birth").value,
-      document.querySelector("#adresse").value,
-      document.querySelector("#mail").value
-    );
+        }
 
-    const contactList = contactStore.getList();
+        else {
+            $(".modal-title").text("Error");
+            $(".modal-body").html(error)
+            $('#myModal').modal("show");
+        }
 
-    document.querySelector("table tbody").innerHTML = "";
+        
 
-    for (var contact of contactList) {
-      document.querySelector("table tbody").innerHTML += `
-        <tr>
-            <td> ${contact.name}</td>
-            <td> ${contact.firstname}</td>
-            <td> ${contact.date}</td>
-            <td> <a href="https://maps.google.com/?q=${contact.adress}">${contact.adress}</a></td>
-            <td> <a href="mailto:${contact.mail}">${contact.mail}</a></td>
-        <tr>`;
-    }
-  });
-};
+    });
+
+
+
+});
